28f5f64601a56a3e3e2819c5ad942164d246e60b
Ben Batschelet <ben@codeup.com>
2015-01-29 16:07:39 -0600

    Added parks and say hello views & routes

---
+++ app/views/sayhello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello <?= $name; ?></h1>
+    <p>I hear you're <?= $age; ?> years old!</p>
+</body>
+</html>
