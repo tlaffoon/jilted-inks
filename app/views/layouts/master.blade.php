63be03643e0a475782a14d442bc42968a389845f
Ben Batschelet <ben@codeup.com>
2015-02-02 15:41:32 -0600

    Added Twitter Bootstrap & pagination. (I'm sorry. I'm so so sorry)

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,13 +1,28 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>My Laravel Blog</title>
+    
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    
+    @yield('top-script')
 </head>
 <body>
-
-
-    @yield('content')
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    
+    <script type="text/javascript" src="/js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
+    
+    @yield('bottom-script')
 </body>
 </html>
--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -9,4 +9,6 @@
     </article>
 @endforeach
 
+{{ $posts->links() }}
+
 @stop
