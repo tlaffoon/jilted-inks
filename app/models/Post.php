35769c07bc032a15c8df264636f5ba91cd284bee
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-03 21:32:23 -0600

    Updated models to use BaseModel, Accessors, and Mutators.

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,9 +1,16 @@
 <?php
 
+use Carbon\Carbon;
+
 class Post extends Eloquent
 {
     public static $rules = array(
         'title' => 'required|max:100',
         'body'  => 'required'
     );
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = ucfirst($value);
+    }
 }
