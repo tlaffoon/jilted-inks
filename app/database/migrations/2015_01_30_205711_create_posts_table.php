1def8da402dd6482b7b39be4991d18d72a8963da
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-06 09:41:34 -0600

    Added slug field to createPostTable migration.

--- app/database/migrations/2015_01_30_205711_create_posts_table.php
+++ app/database/migrations/2015_01_30_205711_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration {
         {
             $table->increments('id');
             $table->string('title', 100);
+            $table->string('slug');
             $table->text('body');
             $table->timestamps();
         });
