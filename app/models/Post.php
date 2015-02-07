d830cbd8df4e888b21c3326ad613430fda2f96df
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-07 08:04:17 -0600

    Updated post model to validate slugs.

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,7 +7,7 @@ class Post extends Eloquent
     public static $rules = array(
         'title' => 'required|max:100',
         'body'  => 'required',
-        'slug'  => 'required|alpha_dash:unique:posts'
+        'slug'  => 'required|alpha_dash|unique:posts'
     );
 
     public function setTitleAttribute($value)
