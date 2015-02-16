1a59a6c6eebb05929c585f88881bf38e83525c1e
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-15 19:31:07 -0600

    Refactored autocomplete form to use laravel blade syntax.

--- app/views/autocomplete.blade.php
+++ app/views/autocomplete.blade.php
@@ -1,51 +1,24 @@
 @extends('layouts.awesome')
 
 @section('css')
-<style>
-   #controls {
-     position: relative;
-     width: 480px;
-   }
-   #autocomplete {
-     position: absolute;
-     top: 0px;
-     left: 0px;
-     width: 99%;
-   }
-   .label {
-     text-align: right;
-     font-weight: bold;
-     width: 100px;
-     color: #303030;
-   }
-   #address {
-     background-color: #fff;
-     width: 480px;
-     padding-right: 2px;
-     position: relative;
-     top: 40px;
-     left: 0px;
-   }
-   #address td {
-     font-size: 10pt;
+<style type="text/css">
+    .no-padding-left {
+        padding-left: 0px;
+    }
 
-   }
-   .field {
-     width: 99%;
-   }
-   .slimField {
-     width: 80px;
-   }
-   .wideField {
-     width: 200px;
-   }
- </style>
+    .no-padding-right {
+        padding-right: 0px;
+    }
+
+
+</style>
 @stop
 
 @section('topscript')
     <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
     <script>
+    
     // This example displays an address form, using the autocomplete feature
     // of the Google Places API to help users fill in the information.
 
@@ -60,8 +33,10 @@
     };
 
     function initialize() {
+
       // Create the autocomplete object, restricting the search
       // to geographical location types.
+      
       autocomplete = new google.maps.places.Autocomplete(
           /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
           { types: ['geocode'] });
@@ -100,8 +75,7 @@
     function geolocate() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-          var geolocation = new google.maps.LatLng(
-              position.coords.latitude, position.coords.longitude);
+          var geolocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           var circle = new google.maps.Circle({
             center: geolocation,
             radius: position.coords.accuracy
@@ -116,45 +90,53 @@
 @stop
 
 @section('content')
-    <h2 class="page-header">Autocomplete Example</h2>
+    <h2 class="page-header">Google Maps JavaScript API v3 <small>Address Autocomplete</small></h2>
 
-    <div class="col-md-12">
-        
-        <input id="autocomplete" placeholder="Enter your address" type="text" class="form-group form-control"></input>
+    <div class="col-md-6">
 
-        <table id="address">
-          <tr>
-            <td class="label">Street address</td>
-            <td class="slimField"><input class="field" id="street_number"
-                  disabled="true"></input></td>
-            <td class="wideField" colspan="2"><input class="field" id="route"
-                  disabled="true"></input></td>
-          </tr>
-          <tr>
-            <td class="label">City</td>
-            <td class="wideField" colspan="3"><input class="field" id="locality"
-                  disabled="true"></input></td>
-          </tr>
-          <tr>
-            <td class="label">State</td>
-            <td class="slimField"><input class="field"
-                  id="administrative_area_level_1" disabled="true"></input></td>
-            <td class="label">Zip code</td>
-            <td class="wideField"><input class="field" id="postal_code"
-                  disabled="true"></input></td>
-          </tr>
-          <tr>
-            <td class="label">Country</td>
-            <td class="wideField" colspan="3"><input class="field"
-                  id="country" disabled="true"></input></td>
-          </tr>
-        </table>
+        <div class="row">
+            {{ Form::label('autocomplete', null)}}
+            {{ Form::text('autocomplete', null, array('class' => 'form-group form-control', 'placeholder' => 'Enter your address...')) }}
+        </div>
+       
+        <div class="row">
+            <div class="col-md-4 no-padding-left">
+                {{ Form::label('street_number', 'Street Number') }}
+                {{ Form::text('street_number', null, array('class' => 'form-group form-control', 'disabled' => true)) }}
+            </div>
+
+            <div class="col-md-8 no-padding-right">
+                {{ Form::label('street_name', 'Street Name') }}
+                {{ Form::text('street_name', null, array('id' => 'route', 'class' => 'form-group form-control', 'disabled' => true)) }}
+            </div>
+        </div>
+
+        <div class="row">
+            {{ Form::label('city', 'City') }}
+            {{ Form::text('city', null, array('id' => 'locality', 'class' => 'form-group form-control', 'disabled' => true)) }}
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 no-padding-left">
+                {{ Form::label('state', 'State') }}
+                {{ Form::text('state', null, array('id' => 'administrative_area_level_1', 'class' => 'form-group form-control', 'disabled' => true)) }}
+            </div>
+
+            <div class="col-md-8 no-padding-right">
+                {{ Form::label('zip', 'Zip') }}
+                {{ Form::text('zip', null, array('id' => 'postal_code', 'class' => 'form-group form-control', 'disabled' => true)) }}
+            </div>
+        </div>
+
+        <div class="row">
+            {{ Form::label('country', 'Country') }}
+            {{ Form::text('country', null, array('id' => 'country', 'class' => 'form-group form-control', 'disabled' => true)) }}
+        </div>
+
     </div>
 
-    <hr>
-
-    <div class="col-md-12">
-
+    <div class="col-md-6">
+        <div id="map-canvas"></div>
     </div>
 
 @stop
@@ -162,9 +144,10 @@
 @section('bottomscript')
 <script type="text/javascript">
     $(document).ready(function () {
+        
         initialize();
-
         $('#autocomplete').focus(geolocate);
+
     });
 </script>
 @stop
