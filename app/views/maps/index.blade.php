44c4a62fde881d867b4f53501e0a71124cd774e9
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-18 23:21:01 -0600

    Updated views.

--- app/views/maps/index.blade.php
+++ app/views/maps/index.blade.php
@@ -18,7 +18,9 @@
             <li><a href="/autocomplete">Autocomplete Form</a></li>
             <li><a href="/geocode">Geocoding An Address</a></li>
             <li><a href="/markers">Plotting Multiple Markers</a></li>
-            <li><a href="/search">Performing Ajax Requests and Searching For Addresses</a></li>
+            <li><a href="/ajax">Performing Ajax Requests</a></li>
+            <li><a href="/search">Searching For Addresses</a></li>
+            <li><a href="/distance-query">Distance Queries Using MySQL</a></li>
         </ul>
 
         <p>This is an additional resource which can help you customize your map, and its behavior:
--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -17,6 +17,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             
             <ul class="nav navbar-nav navbar-left">
+                <li><a href="/posts">Posts</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/resume">Resume</a></li>
                 <li><a href="/portfolio">Portfolio</a></li>
