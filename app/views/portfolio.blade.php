cdab1846da109d9e8a34b6fb0ca4af40bdf38523
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-23 16:44:21 -0600

    Updated.

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -12,15 +12,18 @@
     <h2 class="page-header">Portfolio</h2>
 
     <div class="row">
-        <div class="col-md-4">
+        <!-- <div class="col-md-4">
             <a href="/whack-a-mole">
                 <img src="/includes/img/cat_whack_a_mole.png" class="img-responsive img-rounded img-box">
             </a>
-        </div>
+        </div> -->
         <div class="col-md-4">
             <a href="/gmaps/">
                 <img src="/includes/img/gmaps_logo.png" class="img-responsive img-rounded img-box">
             </a>
+        </div>
+        <div class="col-md-4">
+            <img src="http://placehold.it/220x220" class="img-responsive img-rounded img-box">
         </div>
         <div class="col-md-4">
             <img src="http://placehold.it/220x220" class="img-responsive img-rounded img-box">
