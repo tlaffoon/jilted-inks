af2e44342c7cf08ccf0a12e03b39252b2b9c1ff3
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-03-01 17:13:50 -0600

    Attempted much refactoring of character limit on description field.  Achieved much revert to previous code.

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -79,25 +79,25 @@ $(document).ready(function() {
     // End slug generation
 
     // Limit characters in description field
-    var charLimit = $('#characterLimit');
+    var charLeft = $('#characterLeft');
     var descField = $('#description');
-
-    charLimit.text('400 characters left.');
+    charLeft.text('400 characters left.');
     descField.on("keyup focus", function () {
         var max = 400;
         var descValue = $(this).val().slice(0, max);
         var len = $(this).val().length;
-
         if (len >= max) {
             descField.val(descValue);
-            charLimit.addClass('text-danger');
-            charLimit.text('You have reached the character limit.');
+            charLeft.addClass('text-danger');
+            charLeft.text('You have reached the character limit.');
         } else {
             var ch = max - len;
-            charLimit.removeClass('text-danger');
-            charLimit.text(ch + ' characters left.');
+            charLeft.removeClass('text-danger');
+            charLeft.text(ch + ' characters left.');
         }
     });
+
 });
+
 </script>
 @stop
